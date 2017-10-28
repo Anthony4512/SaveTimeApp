410fc8847a6587b0d312d5894750661cefa35466
Anthony Mirely <anthonymirely22@gmail.com>
2017-10-28 14:29:45 -0400

    created some classes for GUI

--- src/AppMain/Tester.java
+++ src/AppMain/Tester.java
@@ -1,8 +1,10 @@
 package AppMain;
 
+import DataClasses.Appointment;
 import DataClasses.Client;
 import DataClasses.Professional;
 
+import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,24 +17,39 @@ class Tester
 
         SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
         Date DOB = null;
+
+        Date appDate = null;
+
+        Time startTime = null;
+        Time endTime = null;
+
         try {
             DOB = format.parse("08/11/1986");
+            appDate = format.parse("12/12/2017");
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
 
         System.out.println("hello world");
-      
         System.out.println("Executed with no problems");
 
 
         Client client1 = new Client("Jose", "Calderon", "jcalderon", "00",
                 CurrentDay, DOB, "jcalderon@gmail.com", "100 Elliot st", "Haverhill", "MA", "01844",
-                "USA", "978-835-3117");
+                "USA", "9788353117");
+
         Professional professional1 = new Professional("Manuel","Castro", "mcastro", "01",
                 CurrentDay,DOB, "mcastro@gmail.com", "100 Elliot st", "Haverhill", "MA", "01844",
-                "USA", "978-835-7788");
+                "USA", "9788357788");
+
+//        Appointment appointment = new Appointment(true, false, "mcastro", );
+
+        Appointment clientRequestedApp = client1.requestAppointment(professional1, appDate, startTime, endTime);
+
+        System.out.println(clientRequestedApp.getTimeStart());
+        System.out.println(clientRequestedApp.getTimeStop());
+
 
 
         System.out.println(client1.getDob().toString());
--- src/DataClasses/Client.java
+++ src/DataClasses/Client.java
@@ -1,5 +1,6 @@
 package DataClasses;
 
+import java.sql.Time;
 import java.util.Date;
 
 public class Client extends Person
@@ -7,5 +8,12 @@ public class Client extends Person
 
     public Client(String firstName, String lastName, String userName, String ID, Date date, Date dob, String email, String address, String city, String state, String zipCode, String country, String  phoneNumber) {
         super(firstName, lastName, userName, ID, date, dob, email, address, city, state, zipCode, country, phoneNumber);
+    }
+
+    public Appointment requestAppointment(Professional professional, Date date, Time timeStart, Time timeEnd) {
+        Appointment requestedAppointment = new Appointment(false, false, this.userName, date, timeStart,
+                timeEnd, 0, 0, 0);
+
+        return requestedAppointment;
     }
 }
